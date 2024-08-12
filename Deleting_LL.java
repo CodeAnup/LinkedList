0bf8a972d1ab711017abd4e0b2383d182482d86e
CodeAnup <165381842+CodeAnup@users.noreply.github.com>
2024-08-12 23:52:39 +0530

    Add files via upload

---
+++ Deleting_LL.java
@@ -0,0 +1,53 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data1, Node next1){
+        data=data1;
+        next=next1;
+    }
+    Node(int data1){
+        data=data1;
+        next=null;
+    }
+}
+
+public class Deleting_LL {
+    public static Node convertArr(int arr[]){
+       Node head=new Node(arr[0]);
+       Node mover=head;
+
+       for(int i=1;i<arr.length;i++){
+           Node temp=new Node(arr[i]);
+           mover.next=temp;
+           mover=temp;
+       }
+       return head;
+    }
+    public static Node removeHead(Node head){
+      Node temp=head;
+      if(head==null){
+          return head;
+      }
+      temp=temp.next;
+      return head;
+    }
+    public static Node removeTail(Node head){
+        Node temp=head;
+        if(head==null || head.next==null){
+            return head;
+        }
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        return temp.next;
+    }
+    public static void main(String[] args) {
+        int arr[]={2,3,4,5,6};
+        Node head=convertArr(arr);
+        head=removeHead(head);
+        System.out.println("The LL after removing the head element is: "+head.data);
+        head=removeTail(head);
+        System.out.println("The LL elements after removing the Tail element is: "+head.data);
+    }
+}
